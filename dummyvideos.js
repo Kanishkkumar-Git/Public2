f5ed120cefd1a5b093b160a2d3012ebc4b44f31e
Kanishkkumar-Git <kanishknew849@gmail.com>
2025-06-25 16:59:07 +0530

    Create dummyvideos.js

---
+++ dummyvideos.js
@@ -0,0 +1,33 @@
+export const videos = [
+  {
+    id: 1,
+    title: "React Hooks Explained",
+    channel: "CodeWithYou",
+    views: "1.2M",
+    time: "2 days ago",
+    thumbnail: "https://i.ytimg.com/vi/Ke90Tje7VS0/hqdefault.jpg",
+  },
+  {
+    id: 2,
+    title: "Flexbox in 10 minutes",
+    channel: "CSSNinjas",
+    views: "860K",
+    time: "1 week ago",
+    thumbnail: "https://i.ytimg.com/vi/JJSoEo8JSnc/hqdefault.jpg",
+  },
+  {
+    id: 3,
+    title: "JavaScript ES6 Crash Course",
+    channel: "DevSimplified",
+    views: "2.4M",
+    time: "3 weeks ago",
+    thumbnail: "https://i.ytimg.com/vi/NCwa_xi0Uuc/hqdefault.jpg",
+  },
+  {
+    id: 4,
+    title: "Tailwind CSS Tutorial",
+    channel: "TailwindMaster",
+    views: "900K",
+    time: "4 days ago",
+    thumbnail: "https://i.ytimg.com/vi/dFgzHOX84xQ/hqdefault.jpg",
+  }
