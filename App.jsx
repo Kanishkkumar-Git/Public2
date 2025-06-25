d5a30093733f99a3c8e1465ec952e12589464694
Kanishkkumar-Git <kanishknew849@gmail.com>
2025-06-25 17:03:17 +0530

    App.jsx

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import WatchLater from "./pages/WatchLater";
+
+export default function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/watch-later" element={<WatchLater />} />
+      </Routes>
+    </Router>
+  );
+}
